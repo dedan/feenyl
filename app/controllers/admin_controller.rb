bc8c698c179c9eed1490fa2e1379b3f00307521c
Stephan Gabler <stephan.gabler@gmail.com>
2010-07-10 15:02:13 +0200

    cosmetics of admin controller
    removed daily reminder link from admin/index

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,74 +1,76 @@
 class AdminController < ApplicationController
-  
+
   layout "music"
-  
-    
-    
+
+
+
   def index
     #redirect_to :action => "list_unauthorized_users"
   end
-  
+
+
   def remind_users
-	 if params[:name] == "admin" and params[:pass] == "hugendubel"
-	 
-		@users = User.find(:all, :conditions => "remind_me = 1")
-		@users.each do |user|  
-		  if (user.reminded == false) and 
-			  (!user.last_post.nil?) and
-			  (Date.today - user.last_post >= DAYS_TO_WAIT)
-					Thread.new(user){|u| Notify.deliver_weekly_reminder(u)} 
-					user.reminded = true
-					user.save
-		  end		
-		end
-	 end
+   if params[:name] == "admin" and params[:pass] == "hugendubel"
+   
+    @users = User.find(:all, :conditions => "remind_me = true")
+    @users.each do |user|  
+      if (user.reminded == false) and 
+        (!user.last_post.nil?) and
+        (Date.today - user.last_post >= DAYS_TO_WAIT)
+          Thread.new(user){|u| Notify.deliver_weekly_reminder(u)} 
+          user.reminded = true
+          user.save
+      end   
+    end
+   end
   end
   
+  # TODO check whether admin (has to work with crone job)
   def lazy_users
-	 @users = User.lazy_users
-	 Thread.new(@users) { |users|  
-		users.each { |user| Notify.deliver_remember(user) }
-    }
-	 
+    @users = User.lazy_users
+    Thread.new(@users) do |users|  
+      users.each { |user| Notify.deliver_remember(user) }
+    end
   end
-  
+
+
   def delete_user
-	 user_to_delete = User.find(params[:id])
- 	 flash[:notice] = "#{user_to_delete.login} deleted !"
-	 Thread.new(user_to_delete) { |actUser| Notify.deliver_deleted(actUser)  }
-	 user = User.find(params[:id]) 
+   user_to_delete = User.find(params[:id])
+   flash[:notice] = "#{user_to_delete.login} deleted !"
+   Thread.new(user_to_delete) { |actUser| Notify.deliver_deleted(actUser)  }
+   user = User.find(params[:id]) 
     user.remove_digest
-	 user.delete_all_posted
-	 User.delete(params[:id])
-	 redirect_to :action => "index"
+   user.delete_all_posted
+   User.delete(params[:id])
+   redirect_to :action => "index"
   end
   
   def authorize_users
     @activation = params["u"]
-	 @users		 = []
-	 params["u"] = nil
-	 unless @activation.nil?
-		@activation.each do |user| 
-		  tmp_user  = User.find(user[0])
-		  @users		<< tmp_user
+   @users    = []
+   params["u"] = nil
+   unless @activation.nil?
+    @activation.each do |user| 
+      tmp_user  = User.find(user[0])
+      @users    << tmp_user
 
-		  if user[1]['activated'] == "1"
+      if user[1]['activated'] == "1"
 
-			 #wenn noch nicht aktiviert, dann jetzt
-			 #und mach ihn zum htacces user
-			  if (tmp_user.activated == false)
-				  tmp_user.activated = 1
-				  tmp_user.make_htaccess_users
-				  tmp_user.save
-				  Thread.new(tmp_user) { |actUser|
-						Notify.deliver_activated(actUser, current_user)
-				  }
-			  end
-		  end
-		end
-	 else
-		redirect_to :action => "list_users"
-	 end
+       #wenn noch nicht aktiviert, dann jetzt
+       #und mach ihn zum htacces user
+        if (tmp_user.activated == false)
+          tmp_user.activated = 1
+          tmp_user.make_htaccess_users
+          tmp_user.save
+          Thread.new(tmp_user) { |actUser|
+            Notify.deliver_activated(actUser, current_user)
+          }
+        end
+      end
+    end
+   else
+    redirect_to :action => "list_users"
+   end
   end
   
   def list_unauthorized_users 
