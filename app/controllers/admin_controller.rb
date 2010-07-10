7fcf185d919e0eb36c0b713a792b1a7860fbd805
Stephan Gabler <stephan.gabler@gmail.com>
2010-07-10 16:59:06 +0200

    changed how login of admin is checked
    now I use the same system as for the whole music controller.
    overwrite the authorized? method to check whether he
    is really admin in order to acces this controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,11 @@
 class AdminController < ApplicationController
-
+  before_filter :login_required
   layout "music"
 
   def index
-    #redirect_to :action => "list_unauthorized_users"
   end
-
-
+  
+  # this is the method to be called from the crone job
   def remind_users
     if params[:name] == "admin" and params[:pass] == "hugendubel"
       
@@ -23,7 +22,8 @@ class AdminController < ApplicationController
     end
   end
   
-  # TODO check whether admin (has to work with crone job)
+
+  # write an email to all users which have not been active for a while
   def lazy_users
     @users = User.lazy_users
     Thread.new(@users) do |users|  
@@ -33,29 +33,30 @@ class AdminController < ApplicationController
 
 
   def delete_user
-   user_to_delete = User.find(params[:id])
-   flash[:notice] = "#{user_to_delete.login} deleted !"
-   Thread.new(user_to_delete) { |actUser| Notify.deliver_deleted(actUser)  }
-   user = User.find(params[:id]) 
+    user_to_delete = User.find(params[:id])
+    flash[:notice] = "#{user_to_delete.login} deleted !"
+    Thread.new(user_to_delete) { |actUser| Notify.deliver_deleted(actUser)  }
+    user = User.find(params[:id]) 
     user.remove_digest
-   user.delete_all_posted
-   User.delete(params[:id])
-   redirect_to :action => "index"
+    user.delete_all_posted
+    User.delete(params[:id])
+    redirect_to :action => "index"
   end
+  
   
   def authorize_users
     @activation = params["u"]
-   @users    = []
-   params["u"] = nil
-   unless @activation.nil?
-    @activation.each do |user| 
+    @users    = []
+    params["u"] = nil
+    unless @activation.nil?
+      @activation.each do |user| 
       tmp_user  = User.find(user[0])
       @users    << tmp_user
 
       if user[1]['activated'] == "1"
 
-       #wenn noch nicht aktiviert, dann jetzt
-       #und mach ihn zum htacces user
+        #wenn noch nicht aktiviert, dann jetzt
+        #und mach ihn zum htacces user
         if (tmp_user.activated == false)
           tmp_user.activated = 1
           tmp_user.make_htaccess_users
@@ -71,16 +72,21 @@ class AdminController < ApplicationController
    end
   end
   
-  def list_unauthorized_users 
-    redirect_to :action => "list_users", :unauthorized => "yes"
-  end
   
-  def list_users    
+  def list_users
     if params["unauthorized"] == "yes"
       @users = User.find(:all, :conditions => "activated = false") 
     else
       @users = User.find(:all)  
     end
   end
+  
+  
+  private 
+  
+  def authorized?
+    current_user != :false && current_user.login == "admin"
+  end
+  
   
 end
